a9456b4f5d8da4034b3d37f59a315ce2616b0c04
Pavel Pavlov <codedriverun@gmail.com>
2018-01-20 01:42:16 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,33 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application{
+
+    Button button;
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Title of the window");
+        button = new Button();
+        button.setText("click me");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 350);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
